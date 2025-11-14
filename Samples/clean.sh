514a9effb75941b7b43170f037c0d5438d481b95
Tpj-root <trichy_hackerspace@outlook.com>
2025-11-14 12:55:47 +0530

    And Here We Go

--- Samples/clean.sh
+++ Samples/clean.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
+
+# Script Name: clean.sh
+# Description: A brief description of what the script does.
+# Author: Your Name
+# Date: 
+
+# --- Global Variables (Optional) ---
+# EXAMPLE_VAR="Hello World"
+
+# --- Functions ---
+
+function greet_user() {
+  local name="$1"
+  echo "Hello, $name!"
+}
+
+# --- Main Program ---
+
+# if [[ -z "$1" ]]; then
+#   echo "Usage: $0 <name>"
+#   exit 1
+# fi
+
+echo rm -rf *.log *.aux
 sudo rm -rf *.log *.aux
+
+exit 0
